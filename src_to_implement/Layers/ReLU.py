d22b91ea7fbf7cb9d15fdfc0430b2b17b85227f6
MSatwikR <satwikreddy12963@gmail.com>
2025-07-08 16:08:46 +0200

    tryingto commit

---
+++ src_to_implement/Layers/ReLU.py
@@ -0,0 +1,16 @@
+from Layers.Base import BaseLayer
+import numpy as np
+
+class ReLU(BaseLayer):
+    def __init__(self):
+        super().__init__()
+        self.trainable = False
+
+    def forward(self, input_tensor):
+        self.input_tensor = input_tensor
+        output = input_tensor*(input_tensor > 0)
+        return output 
+
+    def backward(self, error_tensor):
+        output = error_tensor * (self.input_tensor > 0)
+        return output
